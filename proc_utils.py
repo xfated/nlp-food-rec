ff9d2ea22a68753742d7065428fab3cd7249134a
Tay Kai Yang <taykaiyang556@gmail.com>
2021-06-24 23:39:10 +0800

    added ngram search

--- proc_utils.py
+++ proc_utils.py
@@ -8,7 +8,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from collections import Counter
 import re
-
+from nltk.util import ngrams
 '''
 compute tfidf scores of each word in corpus
 @Input args
@@ -19,9 +19,11 @@ words  List[string]
 scores List[double]
 '''
 stopwords = set(stopwords.words('english'))
-ignorewords = set(['i', 'the', 'one', 'this', 'go', 'always', 'it', 'we', 'people', 'rating', 'like'])
+ignorewords = set(['i', 'the', 'one', 'this', 'go', 'always', 'it', 'we', 'people', 'rating', 'like',
+                    'got', 'end', 'would'])
 
 # Check if we want the word
+# Return False if we don't want
 def word_check(word):
     global stopwords
     global ignorewords
@@ -33,7 +35,13 @@ def word_check(word):
     if word in ignorewords:
         return False
     return True
-    
+
+def ngram_check(ngram):
+    for word in ngram:
+        if not word_check(word): # if false, means fail
+            return False
+    return True
+
 def tfIdf(corpus):
     tfIdf_vect = TfidfVectorizer(use_idf=True)
     tfIdf = tfIdf_vect.fit_transform(corpus)
@@ -59,15 +67,23 @@ def common_words(corpus, thres_ratio=0.1):
     num_texts = len(corpus)
 
     # Extract words
-    tokenizer = RegexpTokenizer(r'\w+')
-    words = [str.lower(word) for text in corpus for word in tokenizer.tokenize(text) if word_check(str.lower(word))]
+    tokenizer = RegexpTokenizer(r'[A-Za-z0-9]+')
+
+    # unigram
+    unigrams = [str.lower(word) for text in corpus for word in tokenizer.tokenize(text) if word_check(str.lower(word))]
+    # bigram
+    bigrams = [' '.join(bigram) for text in corpus for bigram in ngrams(tokenizer.tokenize(text),2) if ngram_check(bigram)]
+    # trigrams
+    trigrams = [' '.join(trigram) for text in corpus for trigram in ngrams(tokenizer.tokenize(text),3) if ngram_check(trigram)]
+
+    words = unigrams + bigrams + trigrams
     word_count = Counter(words)
     results = [[word, count] for word, count in word_count.items() if count > num_texts*thres_ratio]
     return results
 
 if __name__ == "__main__":
     root = 'C:/Users/User/Documents/portfolio/food-review-scraper/reviewscraper/restaurant_data'
-    jsonfile = 'starbucks.json'
+    jsonfile = '63celsius__asia_square.json'
     filepath = os.path.join(root, jsonfile)
     with open(filepath,'r') as f:
         data = json.load(f)
@@ -77,9 +93,10 @@ if __name__ == "__main__":
     test_data = [review for review, rating in data['reviews']]
 
     ## Test tfidf 
-    # test_data.append(data['about'])
-    # results = tfIdf(test_data)
-    # for res in results:
-    #     print(res)
+    test_data.append(data['about'])
+    results = tfIdf(test_data)
+    print('tfidf:', results)
+
+    ## Test common words
     results = common_words(test_data)
-    print(results)
+    print('common words:', results)
